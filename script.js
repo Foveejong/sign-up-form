811f0ab0bc4614c863d843fb7c38c8168c309be9
foveejong <jovefong62@gmail.com>
2024-04-22 14:39:51 +0800

    Revert "Implement confirm password mechanism"
    This reverts commit 1c642b53daec0e091a9d1bc3786d10879d1a88ae.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const confirmer = document.querySelector("#confirmer");
 const password = document.querySelector("#password");
 const form = document.querySelector("form");
-const matchpass = document.querySelector(".matchpass");
 
 //check if password satisfies conditions
 password.addEventListener("input", (e) => {
@@ -9,18 +8,5 @@ password.addEventListener("input", (e) => {
         password.style.borderColor = "#596D48";
     } else {
         password.style.borderColor = "red";
-    }
-})
-
-form.addEventListener("input", (e) => {
-    if (password.value === confirmer.value) {
-        console.log("right!");
-        confirmer.style.borderColor = "#596D48";
-        matchpass.textContent = "Passwords match!";
-        matchpass.style.color = "#596D48";
-    } else {
-        confirmer.style.borderColor = "red";
-        matchpass.textContent = "Passwords do not match!"
-        matchpass.style.color = "red";
     }
 })
